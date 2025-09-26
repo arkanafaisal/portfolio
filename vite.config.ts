3be12f21928d9381a497f5224205ab123c3961f2
arkanafaisal <arkana.faisal.r@gmail.com>
2025-09-27 06:55:23 +0700

    push

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+// vite.config.js
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio/',
   plugins: [react(), tailwindcss()],
 })
